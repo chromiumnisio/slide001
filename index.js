72a546c9141f43e14524d19513a1ae38d7e95ffa
daishihmr <daishi.hmr@gmail.com>
2013-07-09 18:59:20 +0900

    iroiro

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var PAGES = [
 
     function(c) {
         c.f(40, Color.blue);
-        c.w("自分だけのゲーム素材を作ろう！");
+        c.w("自分だけのエフェクト素材を作ろう！");
         c.f(20, Color.black);
         c.w("2013年7月7日 enchant.js 勉強会", W*0.5, H*0.9);
     },
@@ -94,8 +94,8 @@ var PAGES = [
     },
 
     function(c, b) {
-        c.f(50, Color.red);
-        c.w("イマイチ！");
+        c.f(70, Color.red);
+        c.w("イマイチ...");
         b.entered = function() {
             kuma.removeEventListener("enterframe", kuma.walkMotion);
             kuma.frame = 3;
@@ -104,12 +104,7 @@ var PAGES = [
 
     function(c, b) {
         c.f(30, Color.red);
-        c.w("カッコイイゲーム素材が欲しい！");
-    },
-
-    function(c, b) {
-        c.f(40, Color.blue);
-        c.w("というわけで");
+        c.w("カッコイイエフェクト素材が欲しい！");
         b.entered = function() {
             kuma.on("enterframe", kuma.walkMotion);
         };
@@ -117,15 +112,23 @@ var PAGES = [
 
     function(c, b) {
         c.f(40, Color.blue);
+        c.w("というわけで");
+    },
+
+    function(c, b) {
+        c.f(40, Color.blue);
         c.w("今日は私のやり方をご紹介");
     },
 
+    // ポイント1 加算合成
+    // ポイント2 グラデーション
+    // ポイント3 パクろう
     // 最初はパクリでもいい。むしろ完全再現を目指せ
 ];
 
 var demo = function(context, board, index, url) {
-    context.f(12, Color.green);
-    context.w("demo" + index, W*0.5, H*0.9);
+    context.f(14, Color.blue);
+    context.w("demo" + index + ".html", W*0.5, H*0.9, { underline: true });
     board.on("touchend", function() {
         window.open(url);
     });
@@ -144,6 +147,8 @@ var W = 800*0.8;
 var H = 450*0.8;
 var BOARD_BG;
 var core;
+var layer0;
+var layer1;
 var kuma;
 
 window.onload = function() {
@@ -167,54 +172,57 @@ window.onload = function() {
     })();
 
     core.onload = function() {
-        var scene = core.rootScene;
-        scene.backgroundColor = "black";
-
-        var boards = [];
-        for (var i = 0; i < PAGES.length; i++) {
-            var b = createNew(i);
-            scene.addChild(b);
-            boards.push(b);
-        }
+        
+        core.rootScene.backgroundColor = "black";
+        layer0 = new Group();
+        layer1 = new Group();
+        core.rootScene.addChild(layer0);
+        core.rootScene.addChild(layer1);
 
         var current = ~~location.hash.substring(1);
-        console.log(current);
-
-        for (var i = 0; i < boards.length; i++) {
-            if (i < current) {
-                boards[i].x -= 1600;
-            } else if (i === current) {
-                boards[current].x -= 800;
-            }
-        }
+        var boards = [ createNew(current-1, -800), createNew(current), createNew(current+1, 800) ];
 
         core.on("rightbuttonup", function() {
             if (current >= PAGES.length-1) return;
 
-            boards[current].tl.moveBy(-800, 0, 15, enchant.Easing.QUAD_EASEINOUT);
-            if (boards[current+1]) boards[current+1].tl.moveBy(-800, 0, 15, enchant.Easing.QUAD_EASEINOUT).then(function() {
+            if (boards[0]) layer0.removeChild(boards[0]);
+            boards[1].tl.moveBy(-800, 0, 15, enchant.Easing.QUAD_EASEINOUT);
+            if (boards[2]) boards[2].tl.moveBy(-800, 0, 15, enchant.Easing.QUAD_EASEINOUT).then(function() {
                 if (this.entered) this.entered();
             });
 
-            current += 1;
+            current++;
+
+            boards[0] = boards[1];
+            boards[1] = boards[2];
+            boards[2] = createNew(current+1, 800);
+
             location.hash = "#" + current;
 
-            kuma.scaleX = 1;
+            kuma.scaleX = 3;
         });
 
         core.on("leftbuttonup", function() {
             if (current <= 0) return;
 
-            if (boards[current-1]) boards[current-1].tl.moveBy(800, 0, 15, enchant.Easing.QUAD_EASEINOUT);
-            boards[current].tl.moveBy(800, 0, 15, enchant.Easing.QUAD_EASEINOUT);
-            current -= 1;
+            if (boards[2]) layer0.removeChild(boards[2]);
+            boards[1].tl.moveBy(800, 0, 15, enchant.Easing.QUAD_EASEINOUT);
+            if (boards[0]) boards[0].tl.moveBy(800, 0, 15, enchant.Easing.QUAD_EASEINOUT);
+
+            current--;
+
+            boards[2] = boards[1];
+            boards[1] = boards[0];
+            boards[0] = createNew(current-1, -800);
+
             location.hash = "#" + current;
 
-            kuma.scaleX = -1;
+            kuma.scaleX = -3;
         });
 
         kuma = new Sprite(32, 32);
-        kuma.y = 5;
+        kuma.scale(3, 3);
+        kuma.y = 450 - 70;
         kuma.image = core.assets["images/chara1.png"];
         kuma.on("enterframe", function() {
             this.x = (800-32) * current / PAGES.length;
@@ -223,21 +231,32 @@ window.onload = function() {
             this.frame = ~~(this.age / 20) % 2 + 1;
         };
         kuma.on("enterframe", kuma.walkMotion);
-        scene.addChild(kuma);
+        layer1.addChild(kuma);
     };
     core.start();
 };
 
-var createNew = function(page) {
+var createNew = function(page, x) {
+    console.log("createNew(" + page + ")");
+
+    if (page < 0 || PAGES.length-1 < page) {
+        return;
+    }
+
     var board = new Sprite(W, H);
-    board.x = (800-W)/2 + 800;
+    board.x = (800-W)/2 + ~~x;
     board.y = (450-H)/2;
     board.image = BOARD_BG.clone();
     board.image.context.fillStyle = "black";
     board.image.context.textAlign = "center";
     board.image.context.textBaseline = "middle";
+    board.image.context.shadowColor = "rgba(0,0,0,0.4)";
+    board.image.context.shadowOffsetX = 5;
+    board.image.context.shadowOffsetY = 5;
+    board.image.context.shadowBlur = 5;
+
     board.image.context.f = function(size, color) {
-        size = size || 20;
+        this.fontSize = size = size || 20;
         color = color || "black";
         this.fillStyle = color;
         this.font = size + "px 'serif'";
@@ -245,13 +264,25 @@ var createNew = function(page) {
     board.image.context.prevX = W*0.5;
     board.image.context.prevY = H*0.4;
     board.image.context.lineHeight = H*0.1;
-    board.image.context.w = function(text, x, y) {
+    board.image.context.w = function(text, x, y, option) {
         x = x || this.prevX;
         y = y || this.prevY + this.lineHeight;
         this.fillText(text, x, y);
         this.prevX = x;
         this.prevY = y;
+
+        if (option && option.underline) {
+            var metrics = this.measureText(text);
+            this.fillRect(x - metrics.width/2-1, y + this.fontSize/2, metrics.width+2, 1);
+        }
     };
     PAGES[page](board.image.context, board);
+
+    board.on("enterframe", function() {
+        this.rotation = Math.sin(this.age*0.2) * 2;
+    });
+
+    layer0.addChild(board);
+
     return board;
 };
